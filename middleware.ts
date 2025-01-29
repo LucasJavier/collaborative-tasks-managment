77fbca99de481d5a2af58806337440c07c5c8db3
LucasJavier <lucasbonacci@hotmail.com>
2025-01-29 13:12:12 -0300

    ver cookies

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,8 @@ import { NextResponse, NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
+  const cookieToken = req.cookies.get("__Secure-next-auth.session-token");
+  console.log("Cookie Token:", cookieToken); // Verifica si está en las cookies directamente
   const token = await getToken({ req, secret: process.env.JWT_CLAVE! });
   console.log(token)
   const path = req.nextUrl.pathname;
