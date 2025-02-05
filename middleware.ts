3567a33e38821ed814710f37255166e84fab50d6
LucasJavier <lucasbonacci@hotmail.com>
2025-02-05 09:02:28 -0300

    add logs

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
-  console.log(process.env.NEXTAUTH_SECRET!)
+  console.log("process:", process.env.NEXTAUTH_SECRET!)
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET! });
   console.log("token middleware: ", token)
   const path = req.nextUrl.pathname;
